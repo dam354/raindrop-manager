f5ae7e88ae5ca8b1a07a19dffda58f9f92674c71
Dan Mennella <dmennella34@gmail.com>
2024-05-18 23:17:53 -0400

    chore: Update layout and skeleton components

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -84,7 +84,7 @@ export default function Layout({ children }) {
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
               >
-                <Dialog.Panel className="relative mr--16 flex w-full max-w-xs flex-1">
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
                   <Transition.Child
                     as={Fragment}
                     enter="ease-in-out duration-300"
